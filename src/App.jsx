de49690889daf845d7b5d580db6eab3bbf4ee2cd
Jerson Alexander Moreno Gonzales <moreno.jerson@ficct.uagrm.edu.bo>
2025-06-28 13:52:14 -0400

    Subida inicial del proyecto de feria

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './components/TrafficOptimizer.css';
+import TrafficOptimizer from './components/TrafficOptimizer';
+import './App.css';
+
+function App() {
+  return <TrafficOptimizer />;
+}
+
+export default App;
